ae180ac3a4b7c5fed6ce3e6cefb6b20e5210da9a
zhaoliang <kybetter@163.com>
2019-04-29 19:47:28 +0800

    feat: 修复路由mixin错误

--- src/router/index.js
+++ src/router/index.js
@@ -34,10 +34,16 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   // 如果项目是简单 layout 时，那么需要向父页面发送 postMessage 来调整子页面 iframe 高度。
   if (process.env.VUE_APP_MODE === 'simple') {
-    const components = router.getMatchedComponents(to);
+    const components = Router.getMatchedComponents(to);
     if (components.length === 1) {
+      if (!components[0].mixins) {
+        components[0].mixins = [];
+      }
       components[0].mixins.push(GlobalMixin);
     } else {
+      if (!components[components.length - 1].mixins) {
+        components[components.length - 1].mixins = [];
+      }
       components[components.length - 1].mixins.push(GlobalMixin);
     }
   }
